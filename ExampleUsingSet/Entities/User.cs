4a29144ceb6e1837b7d15d4325b435fefd5e2e33
Julia Muniz <juliabmuniz06@gmail.com>
2024-04-16 08:05:11 -0300

    Add project files.

---
+++ ExampleUsingSet/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExampleUsingSet.Entities {
+    internal class User {
+        public  int Code { get; set; }
+
+        public User() {
+        }
+
+        public User(int code) {
+            Code = code;
+        }
+
+
+        public override int GetHashCode() {
+            return Code.GetHashCode();
+        }
+        public override bool Equals(object obj) {
+            if (!(obj is User)) {
+                return false;
+            }
+            User other = obj as User;
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ ExampleUsingSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExampleUsingSet.Entities;
+
+namespace ExampleUsingSet {
+    internal class Program {
+        static void Main(string[] args) {
+
+            HashSet<User> set = new HashSet<User>();
+            Console.Write("How many students for course A? ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++) {
+                int code = int.Parse(Console.ReadLine());
+
+                set.Add(new User(code));
+            }
+            Console.Write("How many students for course B? ");
+            N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++) {
+                int code = int.Parse(Console.ReadLine());
+
+                set.Add(new User(code));
+            }
+            Console.Write("How many students for course C? ");
+            N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++) {
+                int code = int.Parse(Console.ReadLine());
+
+                set.Add(new User(code));
+            }
+            Console.WriteLine("Total students: " +  set.Count);
+
+
+        }
+    }
+}
